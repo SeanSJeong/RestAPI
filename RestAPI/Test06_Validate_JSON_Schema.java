8822c77e7af0fc6f7675dd4c21756549f8b2f07f
SeanSJeong <39747545+SeanSJeong@users.noreply.github.com>
2022-09-13 13:50:53 -0600

    Add files via upload

---
+++ RestAPI/Test06_Validate_JSON_Schema.java
@@ -0,0 +1,29 @@
+package RestAPI;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+
+import org.testng.annotations.Test;
+
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+public class Test06_Validate_JSON_Schema {
+	// https://www.liquid-technologies.com/online-json-to-schema-converter
+	// https://reqres.in/
+	// https://mvnrepository.com/artifact/io.rest-assured/json-schema-validator/5.2.0
+	// https://github.com/rest-assured/rest-assured/wiki/Usage
+
+	@Test
+	public void testGet() {
+		
+		baseURI = "https://reqres.in/api";
+
+		given().get("users?page=2")
+			.then()
+			.assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schema.json"))
+		.statusCode(200);
+	}
+
+}
