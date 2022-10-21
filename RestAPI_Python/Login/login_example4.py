9b7f8da214cf59b85b878cda94020e14a499da7e
SeanSJeong <39747545+SeanSJeong@users.noreply.github.com>
2022-10-21 15:00:14 -0600

    Update login_example4.py

--- RestAPI_Python/Login/login_example4.py
+++ RestAPI_Python/Login/login_example4.py
@@ -6,10 +6,11 @@ _baseUrl ="http://172.30.144.1"
 
 def getBaseUrl():
     return _baseUrl
+
 def getArdenUrl():
     return _baseUrl + ":9500/" 
 
-url = getArdenUrl()      #url = getBaseUrl()  #url = getBaseUrl() + ":9500/"
+url = getArdenUrl() #url = getBaseUrl() #url = getBaseUrl() + ":9500/"
 payload = {
             'email': "sean@aqua.com",
             'password': "pw"
@@ -19,3 +20,4 @@ response = requests.post("{0}api/arden/user/login".format(url), json=payload, au
 print('Response Code ' + str(response.status_code))
 if response.status_code == 200:
     print('Login succesful: ' + response.text)
+
