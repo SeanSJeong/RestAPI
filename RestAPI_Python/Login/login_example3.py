8948ce0af297713a9d5e22d6129ee2033e223a40
SeanSJeong <39747545+SeanSJeong@users.noreply.github.com>
2022-10-21 14:26:42 -0600

    Create login_example3.py

---
+++ RestAPI_Python/Login/login_example3.py
@@ -0,0 +1,23 @@
+from urllib import response
+import requests
+from requests.auth import HTTPBasicAuth
+
+_baseUrl ="http://172.30.144.1"
+
+def getBaseUrl():
+    return _baseUrl
+
+def getArdenUrl():
+    return _baseUrl + ":9500/" 
+
+url = getBaseUrl()
+
+payload = {
+            'email': "sean@aqua.com",
+            'password': "pw"
+        }
+
+response = requests.post("{0}:9500/api/arden/user/login".format(url), json=payload, auth = HTTPBasicAuth('email','password'))
+print('Response Code ' + str(response.status_code))
+if response.status_code == 200:
+    print('Login succesful: ' + response.text)
